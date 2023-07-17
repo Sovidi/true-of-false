a32301f04e7faa6c05b4d4063231662ad3d4595c
Sovidi <133857264+Sovidi@users.noreply.github.com>
2023-07-17 12:05:45 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,2 +1,15 @@
-$("header").load("./header.html");
+$("header").load("./header.html", headerOpening);
 $("footer").load("./footer.html");
+
+function headerOpening() {
+    const header = document.querySelector("header");
+    const docMain = document.querySelector("main");
+    const menus = document.querySelector(".menus");
+    const fullBttn = document.querySelector(".burger > a");
+    
+    fullBttn.addEventListener("click", addBodyCl);
+    function addBodyCl(){
+        header.classList.toggle("open");
+        docMain.classList.toggle("ds-not");
+    };
+}
